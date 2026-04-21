2cd5f5bd36496a89cae80c779406be4d02ccd47e
HPNT11 <hassans22hh@gmail.com>
2026-04-21 22:27:42 +0200

    Update API URL in environment files for production

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
- // apiUrl: 'https://sunbula-h2b7hzczh0ebfydg.uaenorth-01.azurewebsites.net'
-   apiUrl: 'http://localhost:5142',
+ apiUrl: 'https://sunbula-h2b7hzczh0ebfydg.uaenorth-01.azurewebsites.net'
+   //  apiUrl: 'http://localhost:5142',
 
 };
